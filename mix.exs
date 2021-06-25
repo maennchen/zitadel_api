bd817d962471610e7917f7f9d4382815ed138aa5
Jonatan Männchen <jonatan@maennchen.ch>
2021-06-25 13:00:21 +0200

    Release v1.0.0-rc.6

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ZitadelApi.MixProject do
   @external_resource api_version_file
 
   @api_version api_version_file |> File.read!() |> String.trim()
-  @version "1.0.0-rc.5"
+  @version "1.0.0-rc.6"
 
   def project do
     [
